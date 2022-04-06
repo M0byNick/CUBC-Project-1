683738103eecf6af3b8ba95d05a4cd4645918ff6
djpdim <dimospagkas@yahoo.gr>
2022-04-05 22:16:18 -0400

    initial startup of dimos branch

--- flight.js
+++ flight.js
@@ -1,32 +1,12 @@
-//const axios = require("axios");
-/*
 const options = {
-  method: 'GET',
-  url: 'https://compare-flight-prices.p.rapidapi.com/GetPricesAPI/GetPrices.aspx',
-  params: {SearchID: '<REQUIRED>'},
-  headers: {
-    'X-RapidAPI-Host': 'compare-flight-prices.p.rapidapi.com',
-    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY'
-  }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'best-booking-com-hotel.p.rapidapi.com',
+		'X-RapidAPI-Key': '77e0df06dbmshc3b749f3ded1311p10bac6jsn8c880aa66914'
+	}
 };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-*/
-fetch('https://api.travelpayouts.com/v2/prices/latest?', {
-    'X-Access-Token': '321d6a221f8926b5ec41ae89a3b2ae7b',
-		'X-RapidAPI-Host': 'travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com',
-		'X-RapidAPI-Key': '6c94d0fd0bmsh4df91858ee5e41cp1e51f2jsnc1e1291d77bb',
-    'Access-Control-Allow-Origin': 'no-cors'
-})
-.then(function(response){
-    return response.json()
-    
-})
-.then(function(data)
-{
-    console.log(data)
-})
+fetch('https://best-booking-com-hotel.p.rapidapi.com/booking/best-accommodation?cityName=Berlin&countryName=Germany', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
